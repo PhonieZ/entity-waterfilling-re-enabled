a95369b63202cd163f75ad07fef40f0eb0890301
PhonieZ <PhonieZ@users.noreply.github.com>
2022-07-10 20:12:21 +0100

    Tweaked several folders

--- mod_releases/entity-waterfilling-re-enabled_0.1.0/entity-waterfilling-re-enabled/fix.lua
+++
@@ -1,6 +0,0 @@
- -- iterates through all tiles and sets entity collision check to false
- -- AFTER se post process (as post process is the culprit).
-
-for _, tile in pairs(data.raw.tile) do
-  tile.check_collision_with_entities = false
-end
